429df8b82b4ceee6c46c5113510e7f7af251e49a
humphreynjuguna <humphreynjuguna98@gmail.com>
2023-07-12 16:03:26 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let message;
+  if (distance<=400) {
+     message='This one is on me!';
+  }else if(distance > 2000 && distance<=2500) {
+    message = 'I will gladly take your thirty bucks.';
+}else if(distance > 2500){
+  message='No can do.';
+}
+return message;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case 'generous' :
+      return 'Thank you so much.';
+      case 'not as generous':
+        return 'Thank you.';
+      default:
+        return 'Bye.';
+  }
 }
